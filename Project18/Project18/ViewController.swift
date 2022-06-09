8c52a3d2b7c4479294b1419f58d4314f453c4a6a
 Mekala Vamsi Krishna <68817698+mekala-vamsi-krishna@users.noreply.github.com>
2022-06-09 22:26:00 +0530

    Add files via upload

---
+++ Project18/Project18/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by Mekala Vamsi Krishna on 09/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        print(1, 2, 3, 4, 5, separator: "-")
+        for i in 1...100 {
+            print(i)
+        }
+        
+    }
+
+
+}
+
